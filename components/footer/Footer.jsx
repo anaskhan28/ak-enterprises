2fc5b44f3bf876c0d352e01a24deca5da5515766
anaskhan28 <khananasatique@gmail.com>
2023-06-18 16:30:19 +0530

    homepage done

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,36 +1,33 @@
-import React from 'react'
-import Image from 'next/image'
-import Logo from '../../public/logo.png';
-import './footer.scss'
+import React from "react";
+import Image from "next/image";
+import Logo from "../../public/logo.png";
+import "./footer.scss";
 
 const Footer = () => {
   return (
-   <footer className='footer'>
-    <div className="container">
-      <Image
-      src={Logo}
-      height={60}
-      width={90}
-      className='logo'
-      />
+    <footer className="footer">
+      <div className="container">
+        <Image src={Logo} height={60} width={90} className="logo" />
 
-      <div className="links">
-        <ul>
-          <li>Home</li>
-          <li>Services</li>
-          <li>Contact Us</li>
-          <li>About Us</li>
-          
-        </ul>
+        <div class="links">
+          <div class="group">
+            <ul>
+              <li>Home</li>
+              <li>Services</li>
+            </ul>
+          </div>
+          <div class="group">
+            <ul>
+              <li>Contact Us</li>
+              <li>About Us</li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="line"></div>
-    <div className="right">
-    © 2023 AK Enterprises. All Rights Reserved.
-    </div>
-    
-   </footer>
-  )
-}
+      <div className="line"></div>
+      <div className="right">© 2023 AK Enterprises. All Rights Reserved.</div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
