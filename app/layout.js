96084b3a6f0ffa87ed6c906d9a4fc64f205c6b5f
anaskhan28 <khananasatique@gmail.com>
2023-06-18 11:19:14 +0530

    meta data removed

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+'use client'
 import Head from 'next/head';
 import './globals.scss';
 import { Open_Sans } from 'next/font/google';
@@ -7,33 +8,34 @@ import Footer from '@/components/footer/Footer';
 
 const OpenSans = Open_Sans({ subsets: ['latin'], weight: ['300', '500'] });
 
-export const metadata = {
-  title: 'AK Enterprises',
-  description: 'Solution for all your scrap',
-  // Add more metadata properties as needed
-};
+// export const metadata = {
+//   title: 'AK Enterprises',
+//   description: 'Solution for all your scrap',
+//   // Add more metadata properties as needed
+// };
 
 export default function RootLayout({ children }) {
   return (
     <>
-      <style jsx global>{`
-        html {
-          font-family: ${OpenSans.style.fontFamily};
-        }
-      `}</style>
-      <Head>
+
+    <style jsx global>{`
+            html {
+              font-family: ${OpenSans.style.fontFamily};
+            }
+          `}</style>
+      {/* <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <link rel='icon' href='./favicon.ico'/>
-      </Head>
+      </Head> */}
       <html lang="en">
         <body className={OpenSans.className}>
           <Nav/>
           {children}
           <Footer/>
-        </body>
+          </body>
       </html>
     </>
   );
